1c1ee03d3d3b77278b9a58ac79893fde5a855c83
Ian G <yo.ian.g@gmail.com>
2021-03-10 19:02:45 -0500

    using new Input System

---
+++ Assets/FancyScrollView/Examples/Sources/10_NewInputSystem/Example10.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * FancyScrollView (https://github.com/setchi/FancyScrollView)
+ * Copyright (c) 2020 setchi
+ * Licensed under MIT (https://github.com/setchi/FancyScrollView/blob/master/LICENSE)
+ */
+
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace FancyScrollView.Example10
+{
+    class Example10 : MonoBehaviour
+    {
+        [SerializeField] ScrollView scrollView = default;
+
+        void Start()
+        {
+            var items = Enumerable.Range(0, 20)
+                .Select(i => new ItemData($"Cell {i}"))
+                .ToArray();
+
+            scrollView.UpdateData(items);
+            scrollView.SelectCell(0);
+        }
+
+        static Vector2 navigateUp = new Vector2(0, -1);
+        static Vector2 navigateDown = new Vector2(0, 1);
+        public void Navigate(InputAction.CallbackContext context)
+        {
+            if (context.phase != InputActionPhase.Performed)
+            {
+                return;
+            }
+            var direction = context.ReadValue<Vector2>();
+            if (direction == navigateUp)
+            {
+                SelectPrevious();
+            }
+            else if (direction == navigateDown)
+            {
+                SelectNext();
+            }
+        }
+
+        public void SelectPrevious()
+        {
+            scrollView.SelectPreviousCell();
+        }
+
+        public void SelectNext()
+        {
+            scrollView.SelectNextCell();
+        }
+    }
+}
---
+++ Assets/FancyScrollView/Examples/Sources/10_NewInputSystem/ScrollView.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * FancyScrollView (https://github.com/setchi/FancyScrollView)
+ * Copyright (c) 2020 setchi
+ * Licensed under MIT (https://github.com/setchi/FancyScrollView/blob/master/LICENSE)
+ */
+
+using System.Collections.Generic;
+using UnityEngine;
+using EasingCore;
+
+namespace FancyScrollView.Example10
+{
+    class ScrollView : FancyScrollView<ItemData, Context>
+    {
+        [SerializeField] Scroller scroller = default;
+        [SerializeField] GameObject cellPrefab = default;
+
+        protected override GameObject CellPrefab => cellPrefab;
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            Context.OnCellClicked = SelectCell;
+
+            scroller.OnValueChanged(UpdatePosition);
+            scroller.OnSelectionChanged(UpdateSelection);
+        }
+
+        void UpdateSelection(int index)
+        {
+            if (Context.SelectedIndex == index)
+            {
+                return;
+            }
+
+            Context.SelectedIndex = index;
+            Refresh();
+        }
+
+        public void UpdateData(IList<ItemData> items)
+        {
+            UpdateContents(items);
+            scroller.SetTotalCount(items.Count);
+        }
+
+        public void SelectCell(int index)
+        {
+            if (ItemsSource.Count == 0)
+            {
+                return;
+            }
+
+            var safeIndex = index % ItemsSource.Count;
+            if (index == Context.SelectedIndex)
+            {
+                return;
+            }
+
+            UpdateSelection(index);
+            scroller.ScrollTo(index, 0.35f, Ease.OutCubic);
+        }
+
+        public void SelectPreviousCell()
+        {
+            SelectCell(Context.SelectedIndex + 1);
+        }
+
+        public void SelectNextCell()
+        {
+            SelectCell(Context.SelectedIndex - 1);
+        }
+
+    }
+}
